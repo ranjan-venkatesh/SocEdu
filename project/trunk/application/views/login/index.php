8050bdce9398ab5e435b1f2fbe51b50a9d71f130
ranjan-venkatesh <ranjan.venkatesh92@gmail.com>
2017-10-16 14:17:36 +0200

    create login page

---
+++ project/trunk/application/views/login/index.php
@@ -0,0 +1,17 @@
+<div>
+<div class="container">
+    <div class="col-lg-6 col-lg-3" id="sidebar" role="navigation">
+        <form class="form-signin" action="" id="frmLogin" method="POST">
+            <h2 class="form-signin">Please sign in</h2>
+            <input type="email" class="form-control" placeholder="Email address" required autofocus id="username" name="username">
+            <input type="password" class="form-control" placeholder="Password" required id="password" name="password">
+            <label class="checkbox">
+                <input type="checkbox" value="remember-me"> Remember me
+            </label>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        </form>
+    </div>
+
+</div> <!-- /container -->
+</div>
+<script type="text/javascript" src="/public/js/login.js"></script>
