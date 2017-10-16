2e10daa02c86ceb804f5e8125e57f3397815153d
ranjan-venkatesh <ranjan.venkatesh92@gmail.com>
2017-10-16 14:21:18 +0200

    admin page welcome

---
+++ project/trunk/application/views/admin/index.php
@@ -0,0 +1,6 @@
+<div class="jumbotron">
+    <div class="blog-header">
+        <h1 class="blog-title" style="color: #269abc">Admin Panel</h1></div>
+        <p class="lead blog-description">Welcome Aboard Admin...</p>
+      </div>
+
