15e16bb4e3ffe3e6b3623f323900d4b693325f81
ranjan-venkatesh <ranjan.venkatesh92@gmail.com>
2017-10-16 14:19:22 +0200

    index file for college view

---
+++ project/trunk/application/views/college/index.php
@@ -0,0 +1,41 @@
+<div class="jumbotron">
+    <div class="blog-header">
+        <h1 class="blog-title" style="color: #269abc">College news</h1></div>
+        <p class="lead blog-description">Canara Engineering College</p>
+      </div>
+<div class="container" style="padding: 57px 15px;">
+
+
+      <div class="row">
+
+        <div class="col-sm-8 blog-main">
+
+            <div class="coll-news"></div>
+            <ul class="pager">
+            <li><a href="#">Previous</a></li>
+            <li><a href="#">Next</a></li>
+          </ul>
+              </div><!-- /.blog-main -->
+        <div class="col-sm-3 col-sm-offset-1">
+          <div class="sidebar-module sidebar-module-inset">
+            <h4>Upcoming events</h4>
+            <div class="upcoming-events"></div>
+          </div>
+          <div class="sidebar-module">
+            <h4>Archives</h4>
+            <ol class="list-unstyled">
+              <li><a href="#">January 2014</a></li>
+              <li><a href="#">December 2013</a></li>
+            </ol>
+          </div>
+        </div><!-- /.blog-sidebar -->
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+
+<script type="text/javascript">
+    var collfeeds = <?php echo json_encode($collfeeds); ?>
+</script>
+<script type="text/javascript" src="/public/js/college.js"></script>            
